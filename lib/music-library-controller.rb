c857f47b2cb33bb5356e457ee9ffdfd9077888f5
Winston Pemberton <winstonpemberton@gmail.com>
2020-01-17 00:28:09 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -74,7 +74,7 @@ class MusicLibraryController
     input = gets.chomp.to_i - 1
     songs = Song.all
 
-    if input.between?(1..songs.length)
+    if (1..songs.length).include(input)
       song = Song.all.sort_by(&:name)[input]
     end
 
