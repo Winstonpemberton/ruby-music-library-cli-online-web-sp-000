b95b7b77a43dd7f5b2edbb69ab2e6e9404b8b487
Winston Pemberton <winstonpemberton@gmail.com>
2020-01-17 00:03:12 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -47,6 +47,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.chomp
 
+    artist = Artist.find_by_name(input)
     sorted_artist_songs = Artist.songs.sort_by(&:name)
     sorted_artist_songs.each_with_index do |song, index|
       puts "#{index + 1}. #{song.name} - #{song.genre.name}"
